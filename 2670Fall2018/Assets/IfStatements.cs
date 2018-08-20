cea24e4671718ec1c2a13a23d890b61dcd24afe3
Nathan <npbehunin@gmail.com>
2018-08-20 14:26:07 -0600

    Script and Health Examples
    C# scripts created in Unity showing examples of different values and classes.

---
+++ 2670Fall2018/Assets/IfStatements.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+
+	public bool LightsOn;
+	public float Speed;
+	
+	// Use this for initialization
+	void Start () {
+		if (!LightsOn && Speed >4)
+		{
+			//do work
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 2670Fall2018/Assets/endGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endGame : MonoBehaviour
+{
+
+	public floatData Health;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Health.Value < 0)
+		{
+			//End Game
+		}
+	}
+}
