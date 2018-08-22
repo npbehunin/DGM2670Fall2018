74bfb31a38298b0dcb3e6c1dabd7b87f97376e08
Nathan <npbehunin@gmail.com>
2018-08-22 16:32:35 -0600

    Scriptable Object Assets
    Tests of scriptable objects. Tested shapes with colors and different prefabs.

---
+++ 2670Fall2018/Assets/PowerUpConfig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PowerUPConfig")]
+public class PowerUpConfig : ScriptableObject
+{
+
+	public GameObject Prefab;
+	public Sprite SpriteImage;
+	public Color SpriteColor;
+
+	public void OnConfigure()
+	{
+		var newPrefab = Instantiate(Prefab);
+		newPrefab.name = this.name; //Extracting the name, so it says "Green Circle", "Purple Diamond", etc.
+
+		//A previous prefab was created with the SpriteRenderer already available, so here we just call the component.
+		var newSprite = newPrefab.GetComponent<SpriteRenderer>();
+		newSprite.sprite = SpriteImage;
+		newSprite.color = SpriteColor;
+	}
+}
---
+++ 2670Fall2018/Assets/scriptableTest.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PrefabData : ScriptableObject {
+    
+    public GameObject Prefab; //Identify that we're using prefabs.
+    public Sprite SpriteImage; //We're naming it "SpriteImage."
+    public Color SpriteColor;
+
+    public void OnConfigure() //Going to configure a powerup.
+    {
+        var newPrefab = Instantiate(Prefab); //We're generating a new prefab here. "Prefab" is the GameObject we made a second ago. Var = any variable/data type.
+        var newSprite = newPrefab.GetComponent<SpriteRenderer>(); //Get component is generic. Sprite rendere isn't a part of the game object, so we wouldn't do newPrefab.SpriteRenderer
+        newSprite.sprite = SpriteImage;
+        newSprite.color = SpriteColor;
+    }
+
+}
