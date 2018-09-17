e27c1f8237fb736d582b2dc4204fc503d5040464
Nathan <npbehunin@gmail.com>
2018-09-17 16:43:04 -0600

    Added flip direction and camera fix
    Character now flips correctly while crouching, and the camera follows without turning around.

---
+++ DGM2670UnityProject/Assets/Scripts/AIAgent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))] //Lets us add a navmeshagent component that gives the AI some basic properties
+
+public class AIAgent : MonoBehaviour
+{
+	private NavMeshAgent agent;
+	public Transform Destination;
+	public Transform PostPoint;
+	private Transform finalDestination;
+
+	private void Start()
+	{
+		agent = GetComponent<NavMeshAgent>(); //Same as above
+		finalDestination = transform;
+	}
+
+	private void OnTriggerEnter(Collider obj)
+	{
+		if(obj.transform == Destination)
+		finalDestination = Destination;
+	}
+
+	private void OnTriggerExit(Collider obj) //if we outrun it, it stops
+	{
+		finalDestination = PostPoint;
+	}
+
+	private void Update()
+	{
+			agent.destination = finalDestination.position; //Transform is the class. transform is a static variable in monobehaviour.
+	}
+}
