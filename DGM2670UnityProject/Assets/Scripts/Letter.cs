41c9d1dd622518d386f1ba81a807d824ac7103b7
Nathan <npbehunin@gmail.com>
2018-10-01 11:31:57 -0600

    Added alphabet letter prefabs

---
+++ DGM2670UnityProject/Assets/Scripts/Letter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ArtisanDream.Words;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Letter")]
+public class Letter : ScriptableObject
+{
+    public Letters.LetterSelection Value;
+}
---
+++ DGM2670UnityProject/Assets/Scripts/Letters.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtisanDream.Words
+{
+    public struct Letters //Struct has less data than a class.
+    {
+        public enum LetterSelection
+        {
+            A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+        }
+    }
+}
