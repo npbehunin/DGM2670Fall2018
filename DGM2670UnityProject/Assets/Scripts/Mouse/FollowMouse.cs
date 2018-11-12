86baa29d1addbe5ac67bc0463374376239045d25
Nathan <npbehunin@gmail.com>
2018-11-12 16:42:05 -0700

    Added follow mouse script

---
+++ DGM2670UnityProject/Assets/Scripts/Mouse/FollowMouse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+
+	public Vector3 tempPos;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		tempPos = Input.mousePosition;
+		transform.position = Camera.main.ScreenToWorldPoint(tempPos);
+		print(Camera.main.ScreenToViewportPoint(tempPos));
+	}
+}
--- DGM2670UnityProject/Assets/Scripts/Store/IntData.cs
+++ DGM2670UnityProject/Assets/Scripts/Store/IntData.cs
@@ -6,6 +6,15 @@ using UnityEngine;
 public class IntData : ScriptableObject
 {
     public int Value; //Int is smaller than float, remember? Int = 1,2,3 Float = 1.1, 1.2, etc.
-    
+
+    public void UpdateValue(int i)
+    {
+        Value += i;
+    }
+
+    public void UpdateValue(IntData data)
+    {
+        Value += data.Value;
+    }
     //The exact same as floatdata, but we're calling our Value "int" instead of "float".
 }
