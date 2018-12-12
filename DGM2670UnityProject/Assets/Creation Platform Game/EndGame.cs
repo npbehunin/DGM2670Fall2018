dbfc2e006d6604b3694151e73e830042482b9f1b
npbehunin <npbehunin@gmail.com>
2018-12-11 19:28:59 -0700

    Added a UnityEvent for quitting the game on Escape Click

--- DGM2670UnityProject/Assets/Creation Platform Game/EndGame.cs
+++ DGM2670UnityProject/Assets/Creation Platform Game/EndGame.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class EndGame : MonoBehaviour 
 {
-
 	public void QuitGame() 
 	{
 		Application.Quit();
---
+++ DGM2670UnityProject/Assets/Creation Platform Game/EndGameEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EndGameEvent : MonoBehaviour
+{
+	public UnityEvent QuitGame;
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			QuitGame.Invoke();
+		}
+	}
+}
